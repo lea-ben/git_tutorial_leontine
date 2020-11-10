fcdee084e2a2366d2a5e49a4f9b1c9b5596d8fc3
lea-ben <73586854+lea-ben@users.noreply.github.com>
2020-11-10 22:27:17 +0200

    1th

---
+++ Di - javascript/java1.js
@@ -0,0 +1,27 @@
+alert("je t'aime")
+
+//5 + "34"
+//console.log(5+"je taime");// 534//
+
+5 - "4"
+console.log (5-"4");
+
+10 % 5
+
+console.log(10%5)
+
+5 % 10
+
+console.log (5%10)
+
+
+console.log ("Java" + "Script");
+console.log (" " + " ");
+
+console.log(" " + 0);
+console.log(true + true);
+console.log(true / false);
+console.log(false + true)
+console.log(false / true);
+console.log (3 - 4);
+console.log("Bob" - "bill");
---
+++ findnemo.js
@@ -0,0 +1,9 @@
+let str1 = "mix";
+let str2 = "pod";
+ 
+
+let newWord = str2.slice(0, 2) + str1.slice2 + " " + str1.slice(0, 2)+ str2.slice(0, 2);
+
+
+
+console.log(newWord);
